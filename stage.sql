a816e0c9a1c7544b344ab30b93ccac216b224832
Patrick <82440798+patrick295@users.noreply.github.com>
2026-07-30 12:09:18 +0100

    Update stage.sql

--- stage.sql
+++ stage.sql
@@ -1,4 +1,7 @@
-  CREATE STAGE raw.poe_stage
+
+--stage and file format
+
+CREATE STAGE raw.poe_stage
   URL = 's3://poe-test-analytics/raw/'
   STORAGE_INTEGRATION = s3_poe_integration
   FILE_FORMAT = raw.csv_format;
